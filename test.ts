e7e08b4cd902a618f8d66676d4ba163ec4478b71
Adam Orczyk <adam.orczyk@gmail.com>
2021-08-23 22:41:45 +0200

    Test.

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-soroban.showNumber(123456789)
+// soroban.showNumber(123456789)
 // soroban.showNumber(3.14)
 // soroban.showNumber(-10)
 // soroban.showNumber(12, 0); soroban.showNumber(15, 3); // Clock format: 12:15
