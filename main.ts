d9e3209cc6a31064b69a931ec60c12b38209b160
Adam Orczyk <adam.orczyk@gmail.com>
2021-08-23 22:36:14 +0200

    Blocks. Fixes. Readme.

--- main.ts
+++ main.ts
@@ -1,56 +1,77 @@
-// --- Displaying numbers as on Soroban abacus ---
-// https://en.wikipedia.org/wiki/Soroban
+/**
+ * Show Soroban Number
+ * Displaying numbers as on Soroban abacus.
+ * Soroban: https://en.wikipedia.org/wiki/Soroban
+ * 
+ * (c) 2021, Adam Orczyk
+ */
 
-// Display digit
-function showSorobanDigit(n:number, col:number){
-    if (n == 0){
-        for (let i = 0; i <= 4; i++){
-            led.unplot(col, i)
+//% block="Soroban" color="#8B4513" weight=100 icon="\uf2a1"
+namespace soroban {
+    /**
+     * Show digit.
+     * @param n the number, eg: 1
+     * @param col the column, eg: 1
+     */
+    //% blockId=soroban_show_digit
+    //% block="show digit $n in column $col"
+    //% n.min=0 n.max=9 n.defl=0
+    //% col.min=1 col.max=5 col.defl=1
+    //% weight=99
+    export function showDigit(n: number, col: number) {
+        if (n == 0) {
+            for (let i = 0; i <= 4; i++) {
+                led.unplot(col, i)
+            }
+        }
+
+        if (n >= 5) {
+            led.plot(col, 0)
+            n = n - 5
+        }
+
+        for (let i = 1; i <= n; i++) {
+            led.plot(col, i)
         }
     }
 
-    if (n >= 5){
-        led.plot(col, 0)
-        n = n - 5
-    }
+    /**
+     * Show number. When column is not given clears the screen and displays number part which fit the screen.
+     * @param n the number, eg: 3.14
+     * @param col the starting column, eg: 0
+     */
+    //% blockId=soroban_show_number
+    //% block="show number $n || column $col"
+    //% col.min=0 col.max=4
+    //% weight=99
+    export function showNumber(n: number, col: number = null) {
+        let nStr = n.toString();
 
-    for (let i = 1; i <= n; i++){
-        led.plot(col, i)
-    }
-}
-
-// Display number
-function showSorobanNumber(n:number, shift?:number){
-    if (shift == undefined){
-        basic.clearScreen()
-    }
-
-    let s = n.toString().split('')
-
-    while (s.length){
-        let col = 5 - s.length
-        if (shift){
-            col -= shift
+        if (nStr.length > 5){
+            nStr = nStr.substr(0,5)
         }
 
-        if (col < 0) {
-            continue
-        }
-        let p = s.shift()
+        let chars = nStr.split('')
 
-        if (p == '.'){
-            led.plot(col, 4)
-        } else if (p == '-'){
-            led.plot(col, 2)
+        let c = 5 - chars.length
+        if (col !== null) {
+            c = col
         } else {
-            let strN = parseInt(p)
-            showSorobanDigit(strN, col)
+            basic.clearScreen()
+        }
+
+        while (c < 5) {
+            let p = chars.shift()
+
+            if (p == '.') {
+                led.plot(c, 4)
+            } else if (p == '-') {
+                led.plot(c, 2)
+            } else {
+                showDigit(parseInt(p), c)
+            }
+
+            c += 1
         }
     }
 }
-
-// Example
-// showSorobanNumber(12345)
-// showSorobanNumber(56789)
-// showSorobanNumber(3.14)
-// showSorobanNumber(-10)
--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
-// tu są testy; nie będzie to kompilowane, gdy ten pakiet będzie używany jako rozszerzenie.
+// soroban.showNumber(123456789)
+// soroban.showNumber(3.14)
+// soroban.showNumber(-10)
+// soroban.showNumber(12, 0); soroban.showNumber(15, 3); // Clock format: 12:15
+// soroban.showNumber(123456789, 1)
