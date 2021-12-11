b736f60ce6a1a6ce6814c3c428724af3ae1b1fcc
Adam Orczyk <adam.orczyk@gmail.com>
2021-12-11 19:28:49 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,9 +1,10 @@
 // soroban.showNumber(123456789)
-soroban.showNumber(3.14, 0, false)
-soroban.showNumber(3.15, Align.Right)
+soroban.showNumber(21)
+// soroban.showNumber(3.14, 0, false)
+// soroban.showNumber(3.15, Align.Right)
 // soroban.showNumber(-10)
 
 // Clock format: 12:15
 // soroban.showNumber(12, 0, false); soroban.showNumber(15, 3, false);
 
-soroban.showNumber(123456789, Align.C2)
+// soroban.showNumber(123456789, Align.C2)
